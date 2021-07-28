de2ad5b6bc966486939ab109305e381f71adc841
TheDudeFromCI <thedudefromci@gmail.com>
2021-07-28 00:46:50 -0700

    Added functional warp pads.

--- src/main/java/net/whg/utils/UtilsPlugin.java
+++ src/main/java/net/whg/utils/UtilsPlugin.java
@@ -4,6 +4,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import net.whg.utils.warp.WarpCommand;
 import net.whg.utils.warp.WarpList;
+import net.whg.utils.warp.WarpListener;
 import net.whg.utils.warp.WarpPadCommand;
 import net.whg.utils.whsculpt.WHSculptCommand;
 
@@ -21,5 +22,8 @@ public class UtilsPlugin extends JavaPlugin {
         getCommand("whsculpt").setExecutor(new WHSculptCommand());
         getCommand("warp").setExecutor(new WarpCommand(warpList));
         getCommand("warppad").setExecutor(new WarpPadCommand(warpList));
+
+        var pm = getServer().getPluginManager();
+        pm.registerEvents(new WarpListener(warpList), this);
     }
 }
---
+++ src/main/java/net/whg/utils/warp/WarpListener.java
@@ -0,0 +1,28 @@
+package net.whg.utils.warp;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+public class WarpListener implements Listener {
+    private final WarpList warpList;
+
+    public WarpListener(WarpList warpList) {
+        this.warpList = warpList;
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent e) {
+        var player = e.getPlayer();
+        var location = player.getLocation();
+        var warpPad = warpList.getWarpPadNear(location);
+        if (warpPad == null)
+            return;
+
+        var warpPoint = warpList.getWarpPoint(warpPad.getWarpPoint());
+        if (warpPoint == null)
+            return; // Uhh, corrupted config file?
+
+        player.teleport(warpPoint.getLocation());
+    }
+}
