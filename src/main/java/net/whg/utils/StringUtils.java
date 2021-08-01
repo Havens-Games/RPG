82839cfec744ebbb5996df2882c178f91f631dc8
TheDudeFromCI <thedudefromci@gmail.com>
2021-08-01 04:33:57 -0700

    Added splitCamelCase String utility.

---
+++ src/main/java/net/whg/utils/StringUtils.java
@@ -0,0 +1,21 @@
+package net.whg.utils;
+
+/**
+ * A collection of string handling utilities.
+ */
+public class StringUtils {
+    private StringUtils() {
+    }
+
+    /**
+     * Splits a camel case string into a human-readable string with spaces.
+     * 
+     * @param s - The string to format.
+     * @return The formatted string.
+     */
+    public static String splitCamelCase(String s) {
+        return s.replaceAll(String.format("%s|%s|%s", "(?<=[A-Z])(?=[A-Z][a-z])", "(?<=[^A-Z])(?=[A-Z])",
+                "(?<=[A-Za-z])(?=[^A-Za-z])"), " ");
+    }
+
+}
