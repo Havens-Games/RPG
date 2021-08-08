5279aaf8d2ce79f5d79f6f7669eed3bc8503cf36
TheDudeFromCI <thedudefromci@gmail.com>
2021-08-08 03:52:55 -0700

    Added plugin argument to util logging.

--- src/main/java/net/whg/utils/MessageUtils.java
+++ src/main/java/net/whg/utils/MessageUtils.java
@@ -2,7 +2,7 @@ package net.whg.utils;
 
 import java.util.logging.Level;
 
-import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 
 /**
  * A collection of utility methods dealing with sending messages to the console
@@ -15,41 +15,45 @@ public final class MessageUtils {
     /**
      * Logs an informational message to the console.
      * 
+     * @param plugin  - The plugin that is logging this message.
      * @param message - The message.
      * @param args    - The message format arguments.
      */
-    public static void logInfo(String message, Object... args) {
-        Bukkit.getLogger().log(Level.INFO, () -> String.format(message, args));
+    public static void logInfo(Plugin plugin, String message, Object... args) {
+        plugin.getLogger().log(Level.INFO, () -> String.format(message, args));
     }
 
     /**
      * Logs a warning message to the console.
      * 
+     * @param plugin  - The plugin that is logging this message.
      * @param message - The message.
      * @param args    - The message format arguments.
      */
-    public static void logWarn(String message, Object... args) {
-        Bukkit.getLogger().log(Level.WARNING, () -> String.format(message, args));
+    public static void logWarn(Plugin plugin, String message, Object... args) {
+        plugin.getLogger().log(Level.WARNING, () -> String.format(message, args));
     }
 
     /**
      * Logs an error message to the console.
      * 
+     * @param plugin  - The plugin that is logging this message.
      * @param message - The message.
      * @param args    - The message format arguments.
      */
-    public static void logError(String message, Object... args) {
-        Bukkit.getLogger().log(Level.SEVERE, () -> String.format(message, args));
+    public static void logError(Plugin plugin, String message, Object... args) {
+        plugin.getLogger().log(Level.SEVERE, () -> String.format(message, args));
     }
 
     /**
      * Logs an error message to the console.
      * 
+     * @param plugin  - The plugin that is logging this message.
      * @param error   - The error that was thrown.
      * @param message - The message.
      * @param args    - The message format arguments.
      */
-    public static void logError(Throwable error, String message, Object... args) {
-        Bukkit.getLogger().log(Level.SEVERE, error, () -> String.format(message, args));
+    public static void logError(Plugin plugin, Throwable error, String message, Object... args) {
+        plugin.getLogger().log(Level.SEVERE, error, () -> String.format(message, args));
     }
 }
